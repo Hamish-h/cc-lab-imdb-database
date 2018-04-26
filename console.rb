0940424fb2dce3f0d9cd0c3422ff75cc5599150f
Colin Tindle <ampersand@underscore.space>
2018-04-26 15:30:06 +0100

    added save method to Movie

---
+++ console.rb
@@ -0,0 +1,10 @@
+require("pry")
+
+require_relative("models/movies")
+require_relative("models/stars")
+require_relative("models/castings")
+
+
+
+bindings.pry
+nil
--- models/movie.rb
+++ models/movie.rb
@@ -1,3 +1,20 @@
-class Movie
+require_relative("db/sql_runner")
 
+class Movie
+  attr_accessor :title, :genre, :rating
+  attr_reader :id
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @genre = options["genre"]
+    @rating = options["rating"].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO movies (title, genre, rating) values ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @rating]
+    movie = SqlRunner.run(sql, values).first()
+    @id = movie["id"].to_i
+  end
 end
